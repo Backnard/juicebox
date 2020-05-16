581bc761952f215be4a4c491621df4771b3417a7
Brian Backes <35049560+Backnard@users.noreply.github.com>
2020-05-16 12:48:12 -0700

    replaced updatePost with Preston's updated version

--- db/index.js
+++ db/index.js
@@ -139,27 +139,24 @@ async function createPost({
 // }
 
 async function updatePost(postId, fields = {}) {
-  // read off the tags & remove that field
+  // read off the tags & remove that field 
   const { tags } = fields; // might be undefined
   delete fields.tags;
 
   // build the set string
-  const setString = Object.keys(fields)
-    .map((key, index) => `"${key}"=$${index + 1}`)
-    .join(", ");
+  const setString = Object.keys(fields).map(
+    (key, index) => `"${ key }"=$${ index + 1 }`
+  ).join(', ');
 
   try {
     // update any fields that need to be updated
     if (setString.length > 0) {
-      await client.query(
-        `
-      UPDATE posts
-      SET ${setString}
-      WHERE id=${postId}
-      RETURNING *;
-    `,
-        Object.values(fields)
-      );
+      await client.query(`
+        UPDATE posts
+        SET ${ setString }
+        WHERE id=${ postId }
+        RETURNING *;
+      `, Object.values(fields));
     }
 
     // return early if there's no tags to update
@@ -169,18 +166,17 @@ async function updatePost(postId, fields = {}) {
 
     // make any new tags that need to be made
     const tagList = await createTags(tags);
-    const tagListIdString = tagList.map((tag) => `${tag.id}`).join(", ");
+    const tagListIdString = tagList.map(
+      tag => `${ tag.id }`
+    ).join(', ');
 
     // delete any post_tags from the database which aren't in that tagList
-    await client.query(
-      `
-    DELETE FROM post_tags
-    WHERE "tagId"
-    NOT IN (${tagListIdString})
-    AND "postId"=$1;
-  `,
-      [postId]
-    );
+    await client.query(`
+      DELETE FROM post_tags
+      WHERE "tagId"
+      NOT IN (${ tagListIdString })
+      AND "postId"=$1;
+    `, [postId]);
 
     // and create post_tags as necessary
     await addTagsToPost(postId, tagList);
