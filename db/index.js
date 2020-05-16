2c0de638659a4f19c182869875e7de8785d3a94c
Brian Backes <35049560+Backnard@users.noreply.github.com>
2020-05-16 12:51:59 -0700

    added getPostsByTagName and updated testDB

--- db/index.js
+++ db/index.js
@@ -332,6 +332,24 @@ async function getPostById(postId) {
   }
 }
 
+async function getPostsByTagName(tagName) {
+  try {
+    const { rows: postIds } = await client.query(`
+      SELECT posts.id
+      FROM posts
+      JOIN post_tags ON posts.id=post_tags."postId"
+      JOIN tags ON tags.id=post_tags."tagId"
+      WHERE tags.name=$1;
+    `, [tagName]);
+
+    return await Promise.all(postIds.map(
+      post => getPostById(post.id)
+    ));
+  } catch (error) {
+    throw error;
+  }
+} 
+
 module.exports = {
   client,
   createUser,
@@ -343,4 +361,5 @@ module.exports = {
   getAllPosts,
   createTags,
   addTagsToPost,
+  getPostsByTagName,
 };
--- db/seed.js
+++ db/seed.js
@@ -12,6 +12,7 @@ const {
   getAllPosts,
   createTags,
   addTagsToPost,
+  getPostsByTagName,
 } = require("./index");
 
 async function createTables() {
@@ -189,6 +190,10 @@ async function testDB() {
       tags: ["#youcandoanything", "#redfish", "#bluefish"]
     });
     console.log("Result:", updatePostTagsResult);
+
+    console.log("Calling getPostsByTagName with #happy");
+    const postsWithHappy = await getPostsByTagName("#happy");
+    console.log("Result:", postsWithHappy);
 
     console.log("Finished database tests!");
   } catch (error) {
