fc20b6be6236adc0f1dc0a2ebca88201a11defde
Brian Backes <35049560+Backnard@users.noreply.github.com>
2020-05-23 12:27:31 -0700

    day 3 finished

--- api/posts.js
+++ api/posts.js
@@ -14,7 +14,7 @@ postsRouter.get('/', async (req, res) => {
     const allPosts = await getAllPosts();
 
     const posts = allPosts.filter(post => {
-      // keep a post if it is either active, or if it belongs to the current user
+      return post.active || (req.user && post.author.id === req.user.id);
     });
 
     res.send({
